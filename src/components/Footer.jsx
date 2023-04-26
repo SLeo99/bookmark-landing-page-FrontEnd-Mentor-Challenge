ee3dd2482fb8e835ba08bdaff6f5525bb2b4947a
SLeo99 <leonelsg1999@gmail.com>
2023-04-26 12:05:18 -0300

    footer logo fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import bookmarkFooterLogo from '../assets/logo-bookmark-white.svg';
 import { FaFacebookSquare, FaTwitter } from "react-icons/fa";
+
 
 export default function Footer() {
   return (
     <footer className='bg-VeryDarkBlue py-10 space-y-11 lg:space-y-0 lg:flex lg:justify-center lg:py-8'>
       <div className="flex flex-col items-center justify-between gap-9 lg:flex-row lg:justify-normal lg:w-8/12 lg:gap-[64px] lg:pl-11">
-        <img src="../src/assets/logo-bookmark-white.svg" alt="" fill='hsl(0, 0%, 100%)' />
+        <img src={bookmarkFooterLogo} alt="bookmark logo" fill='hsl(0, 0%, 100%)' />
         <ul className='flex flex-col gap-6 lg:flex-row lg:gap-11'>
           <a href="/" className='uppercase text-GrayishBlue text-center tracking-wider text-md hover:text-SoftRed transition lg:text-sm'>Features</a>
           <a href="/" className='uppercase text-GrayishBlue text-center tracking-wider text-md hover:text-SoftRed transition lg:text-sm'>Pricing</a>
