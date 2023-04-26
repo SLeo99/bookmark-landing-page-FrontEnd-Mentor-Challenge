90afdbbcedc7dc23b31a9f678e58818943cdc31d
SLeo99 <leonelsg1999@gmail.com>
2023-04-26 12:40:53 -0300

    feature imgs

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -10,19 +10,19 @@ export default function Features() {
     "info": 
     [{
       "name": "Simple Bookmarking",
-      "img": "../src/assets/illustration-features-tab-1.svg",
+      "img": "../assets/illustration-features-tab-1.svg",
       "title": "Bookmark in one click",
       "description": "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites."
     },
     {
       "name": "Speedy Searching",
-      "img": "../src/assets/illustration-features-tab-2.svg",
+      "img": "../assets/illustration-features-tab-2.svg",
       "title": "Intelligent search",
       "description": "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks."
     },
     {
       "name": "Easy Sharing",
-      "img": "../src/assets/illustration-features-tab-3.svg",
+      "img": "../assets/illustration-features-tab-3.svg",
       "title": "Share your bookmarks",
       "description": "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button."
     }]
